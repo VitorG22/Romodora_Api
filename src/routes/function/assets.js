09305fcccdd9644912875691dd1c415dbdc0aebb
VitorG22 <limavitor447@gmail.com>
2025-02-08 14:20:09 -0300

    feat: new Stickers

--- src/routes/function/assets.js
+++ src/routes/function/assets.js
@@ -1,6 +1,12 @@
 async function GetStickers(){
     const stickersList = [
-        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub.png'
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_1.png',
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_2.png',
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_3.png',
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_4.png',
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_5.png',
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_6.png',
+        'https://raw.githubusercontent.com/VitorG22/Romodora_Api/refs/heads/main/assets/stickers/Rub_7.png',
     ]
     return ({
         status: 'success',
