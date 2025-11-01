b6c7ef3e276abe3b4215a33559dd81b8cec6519c
VitorG22 <limavitor447@gmail.com>
2025-11-01 09:01:18 -0300

    change function to get Tiles name

--- src/mapCreationFunctions/assetsJsonCreation.js
+++ src/mapCreationFunctions/assetsJsonCreation.js
@@ -15,7 +15,6 @@ async function AssetsJson() {
                 fs.readdir(`${assetsDir}/${folderName}`, (error, tilesList) => {
                     if (error) return
                     tilesList.forEach(async(tileName, tileIndex) => {
-                        console.log(folderName)
                         assetsObject[folderName].push({
                             path: `https://github.com/VitorG22/Romodora_Api/blob/main/assets/tiles/${folderName}/${tileName}?raw=true`,
                             name: getImageName(tileName),
@@ -34,38 +33,9 @@ async function AssetsJson() {
 }
 
 function getImageName(fileName) {
-    const numberList = '0123456789'
-    let fileNameCopy = fileName.replace('.png', '')
-
-    let tileVariation = ''
-    if (numberList.includes(fileNameCopy[fileNameCopy.length - 1]) == false) {
-        tileVariation = fileNameCopy[fileNameCopy.length - 1]
-        fileNameCopy = fileNameCopy.slice(0, -2)
-    }
-
-    let fileNameSplit = fileNameCopy.split("_")
-    fileNameSplit.pop()
-    fileNameSplit.push(tileVariation)
-    fileNameCopy = fileNameSplit.join(' ').trim()
+    let fileNameCopy = fileName.replace('.png', '').replace('_', " ")
     return fileNameCopy
 }
-
-// function getImageSize(fileName) {
-//     const numberList = '0123456789'
-//     let fileNameCopy = fileName.replace('.png', '')
-
-
-//     if (numberList.includes(fileNameCopy[fileNameCopy.length - 1]) == false) {
-//         fileNameCopy = fileNameCopy.slice(0, -2)
-//     }
-
-//     let fileNameSplit = fileNameCopy.split("_")
-
-
-//     let sizeSplit = fileNameSplit[fileNameSplit.length - 1].split('x')
-//     let size = { x: parseInt(sizeSplit[0]), y: parseInt(sizeSplit[1]) }
-//     return size
-// }
 
 async function getImageSize(folderName,fileName){
     const dimensions = await imageSizeFromFile(`../../assets/tiles/${folderName}/${fileName}`)
