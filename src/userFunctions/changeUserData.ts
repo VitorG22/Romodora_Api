2b0f03e3fa1fa8ac1e59f9c68f389bde32e4fce5
VitorG22 <limavitor447@gmail.com>
2026-01-10 17:48:35 -0300

    update function changeUserData validation

--- src/userFunctions/changeUserData.ts
+++ src/userFunctions/changeUserData.ts
@@ -1,28 +1,30 @@
 import type { Response } from "express";
-import {prisma} from '../../lib/prisma'
+import { prisma } from '../../lib/prisma'
 import hashPassword from '../scripts/hashPassword'
 
 
 export async function changeUserData({ data, user, res }: {
     data: {
-        password: string,
+        password?: string,
         newPassword?: string
-        newUserName?:string
-        newPicture?:string
+        newUserName?: string
+        newPicture?: string
     }, user: { id: string }, res: Response
 }) {
 
     try {
         const userData = await prisma.user.findUniqueOrThrow({ where: { id: user.id } })
 
-        if (userData.password != hashPassword(data.password)) {
+        let newPassword = undefined
+        if (data.newPassword) { newPassword = hashPassword(data.newPassword) }
+
+        if (newPassword != undefined && (data.password == undefined || userData.password != hashPassword(data.password))) {
             res.status(400).json('incorrect Password')
             return
         }
 
 
-        let newPassword = undefined
-        if (data.newPassword) { newPassword = hashPassword(data.newPassword) }
+
         await prisma.user.update({
             where: {
                 id: user.id,
